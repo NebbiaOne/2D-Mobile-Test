62e834339df750cda9dd31899a7f95953e4f9f5c
NebbiaOne <fairhurstdylan42@gmail.com>
2019-03-03 22:09:32 +0200

    Added stuff

---
+++ Mobile 2D Test/Assets/Scripts/Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Vector2 target;
+    private Vector2 position;
+    private Camera maincam;
+    public GameObject TouchTracker;
+
+
+    void Start()
+    {
+        target = TouchTracker.transform.position;
+        maincam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
